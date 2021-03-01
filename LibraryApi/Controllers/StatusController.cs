4b25104be0af78a8d48331b982f10931aa6c9ce0
rgalinas26 <52674802+rgalinas26@users.noreply.github.com>
2021-03-01 13:56:36 -0500

    after lunch monday

---
+++ LibraryApi/Controllers/StatusController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        [HttpGet("status")]
+        public StatusResponse GetTheStatuts()
+        {
+            return new StatusResponse
+            {
+                Message = "Everything is going great. Thanks for asking!",
+                LastChecked = DateTime.Now
+            };
+        }
+    }
+
+    public class StatusResponse
+    {
+        public string Message { get; set; }
+        public DateTime LastChecked { get; set; }
+    }
+}
