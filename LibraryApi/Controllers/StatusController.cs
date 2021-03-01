5f2887fe8f90131340edee1a4a41c34a955dc738
rgalinas26 <52674802+rgalinas26@users.noreply.github.com>
2021-03-01 15:19:36 -0500

    before first day break. talked about ways to get data into the api

--- LibraryApi/Controllers/StatusController.cs
+++ LibraryApi/Controllers/StatusController.cs
@@ -17,6 +17,43 @@ namespace LibraryApi.Controllers
                 LastChecked = DateTime.Now
             };
         }
+
+        // the :int makes the route expect an int
+        [HttpGet("customers/{customerId:int}")]
+        public ActionResult GetInfoAboutCustomer(int customerId)
+        {
+            return Ok($"Getting info about customer {customerId}");
+        }
+
+        // straight up data in the URL "heirarchical"
+        [HttpGet("blogs/{year:int}/{month:int:min(1):max(12)}/{day:int}")]
+        public ActionResult GetBlogPosts(int year, int month, int day)
+        {
+            return Ok($"Getting blogs for {month}-{day}-{year}");
+        }
+
+        //query string data entry
+        [HttpGet("employees")]
+        public ActionResult GetEmployees([FromQuery]string department = "All")
+        {
+            var response = new GetEmployeesResponse
+            {
+                Data = new List<string> { "Joe", "Ron" },
+                Department = department
+            };
+            return Ok(response);
+        }
+
+        [HttpGet("whoami")]
+        public ActionResult WhoAmI([FromHeader(Name ="User-Agent")]string userAgent)
+        {
+            return Ok($"I have no idea, but you are running {userAgent}");
+        }
+    }
+    public class GetEmployeesResponse
+    {
+        public List<string> Data { get; set; }
+        public string Department { get; set; }
     }
 
     public class StatusResponse
